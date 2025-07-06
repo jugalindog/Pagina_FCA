4832942ad9c3bb09984ee68d846aa051312df30a
jugalindog <jugalindog@unal.edu.co>
2025-07-06 00:50:29 -0500

    cambio error rout.main en app.py.txt

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from datetime import timedelta
-from routes.main import main as main_blueprint
+from main import main as main_blueprint
 
 def create_app():
     app = Flask(__name__)
